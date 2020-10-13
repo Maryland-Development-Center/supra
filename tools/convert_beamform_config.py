8f3b2f341b62b1a4110ea149f8e0ff39ff896acb
Kyle <kmonte@umd.edu>
2020-10-13 09:17:28 -0400

    maybe better scanline layout?

--- tools/convert_beamform_config.py
+++ tools/convert_beamform_config.py
@@ -7,15 +7,15 @@ pre = json.load(open(config))
 
 # constants/etc
 to_update = {
-    "depth": 52.5397,
+    "depth": 25,
     "elementLayout": {"x": 128, "y": 1},
     "numElements": 128,
-    "numReceivedChannels": 127,
+    "numReceivedChannels": 64,
     "numSamples": 2048,
     "numTxScanlines": 128,
     "rxNumDepths": 2048,
-    "samplingFrequency": 7500000,
-    "scanlineLayout": {"x": 128, "y": 1},
+    "samplingFrequency": 30000000,
+    "scanlineLayout": {"x": 127, "y": 1},
 }
 
 out = pre.copy()
@@ -23,7 +23,10 @@ for k, v in to_update.items():
     out[k] = v
 
 out["rxElementPosition"] = pre["rxElementPosition"][:out["numElements"]]
-out["rxScanlines"] = pre["rxScanlines"][::2]
+out["rxScanlines"] = pre["rxScanlines"][::2][:-1]
+for sl in out["rxScanlines"]:
+    sl["txParameters"] = sl["txParameters"][:2]
 
 print(f"number of rx scanlines: {len(out['rxScanlines'])}")
+print(f"Writing to {out_file}")
 json.dump(out, open(out_file, "w"))
